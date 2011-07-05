ba3fea1cb51ab2d9d0a6818e8c2fa03d978a9981
Levon Lloyd <levon.lloyd@gmail.com>
2011-07-05 07:21:34 -0400

    Fixed some bugs.  Should work now

--- launch_cluster.py
+++ launch_cluster.py
@@ -8,7 +8,9 @@ def launch_cluster(ami, number, user_data_script):
     """Launch a cluster of rabbit mq nodes on ec2"""
     ec2 = boto.connect_ec2()
     # launch master
-    master_user_data = read_user_data(user_data_script)
+    master_replacements = {}
+    master_replacements['%IS_MASTER%'] = str(1)
+    master_user_data = read_user_data(user_data_script, master_replacements)
     reservation = ec2.run_instances(ami, security_groups=['default'],
                       user_data=master_user_data, instance_type='m1.large',
                       placement=None)
@@ -19,9 +21,14 @@ def launch_cluster(ami, number, user_data_script):
     # launch slaves
     num_slaves = number - 1
     if num_slaves > 0:
+        slave_replacements = {}
+        slave_replacements['%IS_MASTER%'] = str(0)
+        slave_replacements['%RABBIT_MASTER%'] = master_rabbit
+        slave_user_data = read_user_data(user_data_script, slave_replacements)
         reservation = ec2.run_instances(ami, security_groups=['default'],
-                      user_data=master_user_data, instance_type='m1.large',
-                      placement=None, min_count=num_slaves, max_count=num_slaves)
+                      user_data=slave_user_data, instance_type='m1.large',
+                      placement=reservation.instances[0].placement,
+                      min_count=num_slaves, max_count=num_slaves)
 
 def make_master_name(private_dns):
     """Take an ec2 private dns and turn it into
@@ -41,13 +48,17 @@ def wait_until_running(reservation):
             print "Status is {0}".format(status)
             time.sleep(15)
 
-def read_user_data(user_data_file):
+def read_user_data(user_data_file, replacements):
     """Read a file and put it into a compressed byte string so it
        can be used a the user data string"""
     pass
     raw_string = ''
     with open(user_data_file, 'r') as f:
         raw_string = f.read()
+
+    for (match, replacement) in replacements.items():
+        raw_string = raw_string.replace(match, replacement)
+
 
     result = StringIO.StringIO()
     compressed = gzip.GzipFile(mode='wb',fileobj=result)
--- sh/setup_rabbit.sh
+++ sh/setup_rabbit.sh
@@ -1,11 +1,26 @@
 #!/bin/bash -x
 
+export HOME="/root"
+
+IS_MASTER=%IS_MASTER%
+
 apt-add-repository "deb http://www.rabbitmq.com/debian/ testing main"
 wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
 apt-key add rabbitmq-signing-key-public.asc
 apt-get update
 yes | apt-get install rabbitmq-server
 
+rabbitmqctl stop_app
+rabbitmqctl reset
 rabbitmqctl stop
 s3cmd get --force s3://silverline/.erlang.cookie /var/lib/rabbitmq/.erlang.cookie
+
+if [ $IS_MASTER -ne 1 ]; then
+  cat << EOF > /etc/rabbitmq/rabbitmq.config
+[{rabbit, [{cluster_nodes, ['%RABBIT_MASTER%']}]}].
+EOF
+fi
+
+
+sleep 10
 rabbitmq-server -detached
